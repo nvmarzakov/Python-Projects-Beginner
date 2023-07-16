0a008b870c774f198525b5341f879a9e738a0807
Nikolay Marzakov <114495254+nvmarzakov@users.noreply.github.com>
2023-07-16 22:35:58 +0300

    Update main.py

--- Projects/Intermediate/snake_game/main.py
+++ Projects/Intermediate/snake_game/main.py
@@ -36,16 +36,20 @@ while game_is_on:
 
     # Detect collusion with wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
 
     # Detect collusion with tail
     for segment in snake.snake_body[1:]:
 
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+            scoreboard.reset()
+            snake.reset()
+
 
 screen.exitonclick()
+
+
+
 
 
