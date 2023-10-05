e9221e5a2347f7ee0babed765170404eb12ff15c
Nikolay Marzakov <114495254+nvmarzakov@users.noreply.github.com>
2023-10-05 22:27:56 +0300

    Update main.py

--- Projects/Intermediate/guess_the_provinces/main.py
+++ Projects/Intermediate/guess_the_provinces/main.py
@@ -8,19 +8,37 @@ image = "Bulgaria.gif"
 turtle.addshape(image)
 turtle.shape(image)
 
+# see the location o
+# def get_mouse_click_coor(x, y):
+#     print(x, y)
+#
+#
+# turtle.onscreenclick(get_mouse_click_coor)
+#
+# turtle.mainloop()
+
 data = pandas.read_csv("28_provinces.csv")
 all_provinces = data.province.to_list()
 guessed_provinces = []
 
+
 while len(guessed_provinces) < 28:
     # taking input
     answer = turtle.textinput(
-      title=f"{len(guessed_provinces)}/28 Provinces Correct",
-      prompt="Do you know another province?")\
-    .title()
+        title=f"{len(guessed_provinces)}/28 Provinces Correct",
+        prompt="Do you know another province?")\
+        .title()
 
     # print answer
     print(f"{answer} Province")
+    if answer == 'Exit':
+        missing_provinces = []
+        for province in all_provinces:
+            if province not in guessed_provinces:
+                missing_provinces.append(province)
+        new_data = pandas.DataFrame(missing_provinces)
+        new_data.to_csv("provinces_to_learn.csv")
+        break
 
     if answer in all_provinces and answer not in guessed_provinces:
         guessed_provinces.append(answer)
@@ -29,6 +47,7 @@ while len(guessed_provinces) < 28:
         t.penup()
         province_data = data[data.province == answer]
         t.goto(float(province_data.x), float(province_data.y))
-        t.write(answer)
+        t.write(f'{answer} province')
 
-screen.exitonclick()
+
+# provinces_to_learn.csv
